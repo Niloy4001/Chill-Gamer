7f9f073d182e200d9566583745625edda50a310b
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-12-05 21:55:10 +0600

    404 page added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
       <div>
         <h1 className="text-5xl font-semibold text-center">Page Not Found</h1>
         <div className="flex justify-center mt-5">
-          <button 
-            onClick={()=>navigate(-1)}
-            className="btn btn-primary">Go back</button>
+          <button onClick={() => navigate(-1)} className="btn btn-primary">
+            Go back
+          </button>
         </div>
       </div>
     </div>
